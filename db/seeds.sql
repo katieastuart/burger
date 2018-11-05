e5373f3b14037d8982785a8bd8f4e106f0a41361
kstuart <katieannestuart@gmail.com>
2018-11-04 23:11:46 -0700

    working app need to fix buttons still

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Double');
+INSERT INTO burgers (name) VALUES ('Dragonslayer');
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (name, devoured) VALUES ('McChicken', true);
+INSERT INTO burgers (name) VALUES ('Quarter Pounder');
